69287241e08e78b635cc24ffb5dea7b81cd253cb
Beg <a.behaghel@inato.com>
2020-08-14 20:40:42 +0200

    🎨 Format src files

--- src/agility/components/AgilityPageTemplate.js
+++ src/agility/components/AgilityPageTemplate.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react"
 
-const AgilityPageTemplate = (props) => {
-    const pageTemplateName = props.page.templateName.replace(/[^0-9a-zA-Z]/g, '');
-    const PageTemplateComponentToRender = require(`../../pageTemplates/${pageTemplateName}.js`).default;
+const AgilityPageTemplate = () => {
+  const pageTemplateName = props.page.templateName.replace(/[^0-9a-zA-Z]/g, "")
+  const PageTemplateComponentToRender = require(`../../pageTemplates/${pageTemplateName}.js`)
+    .default
 
-    //get the page template name that we need to render
-    
-    //const PageTemplateComponentToRender = props.pageTemplates[pageTemplateName];
-    delete props.pageTemplates;
-    return (
-        <PageTemplateComponentToRender {...props} />
-    );
+  //get the page template name that we need to render
+
+  //const PageTemplateComponentToRender = props.pageTemplates[pageTemplateName];
+  delete props.pageTemplates
+  return <PageTemplateComponentToRender {...props} />
 }
 
-export default AgilityPageTemplate;
+export default AgilityPageTemplate
