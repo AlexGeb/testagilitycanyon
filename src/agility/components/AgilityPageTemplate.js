8e793a4b6d3978a5e15b688e31062ad3d61ceb13
Beg <a.behaghel@inato.com>
2020-08-14 20:41:30 +0200

    🎨 Format other js files

--- src/agility/components/AgilityPageTemplate.js
+++ src/agility/components/AgilityPageTemplate.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const AgilityPageTemplate = () => {
+const AgilityPageTemplate = (props) => {
   const pageTemplateName = props.page.templateName.replace(/[^0-9a-zA-Z]/g, "")
   const PageTemplateComponentToRender = require(`../../pageTemplates/${pageTemplateName}.js`)
     .default
